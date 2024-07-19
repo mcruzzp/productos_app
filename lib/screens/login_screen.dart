5904956d64028c036a90bce3bf9943476c8b3404
mcruzzp <m.ruz.zp@gmail.com>
2024-07-19 19:08:47 +0200

    sube

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -23,16 +23,55 @@ class LoginScreen extends StatelessWidget {
                     
                     SizedBox(height: 30,),
 
-                    Text('Formulario')
-
+                    _LoginForm()
                   ],)
               ),
+              SizedBox(height: 50,),
+              Text('Crear un nueva cuenta', style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold))
 
 
 
             ],)
         )
       )
+    );
+  }
+}
+
+class _LoginForm extends StatelessWidget {
+  const _LoginForm({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Form(
+        //TODO mantener la referencia al KEY
+        child: Column(
+          children: [
+            TextFormField(
+              autocorrect: false,
+              keyboardType: TextInputType.emailAddress,
+              decoration: InputDecoration(
+                enabledBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple
+                  )
+                ),
+                focusedBorder: UnderlineInputBorder(
+                  borderSide: BorderSide(
+                    color: Colors.deepPurple,
+                    width: 2
+                  )
+                ),
+                hintText: 'Correo electrónico',
+                labelText: 'Correo',
+                labelStyle: TextStyle(
+                  color: Colors.grey
+                ),
+                prefixIcon: Icon(Icons.alternate_email_sharp, color: Colors.deepPurple)
+              ),
+            )
+          ],),)
     );
   }
 }
