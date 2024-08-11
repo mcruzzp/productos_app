0825e11d4279ba35ec9a9e837f3352499b0c19d7
mcruzzp <m.ruz.zp@gmail.com>
2024-08-11 13:05:16 +0200

    aaaa

---
+++ lib/services/product_service.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:productos_app/models/models.dart';
+
+class ProductsService extends ChangeNotifier {
+final String _baseUrl = 'flutter-varios-c2850-default-rtdb.europe-west1.firebasedatabase.app';
+
+final List<Product> products = [];
+
+}
