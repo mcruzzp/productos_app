06a83a6b51afa9c2333e38d110c8ab880e0eb8fa
mcruzzp <m.ruz.zp@gmail.com>
2024-11-10 13:07:21 +0100

    aaa

--- lib/services/products_service.dart
+++ lib/services/products_service.dart
@@ -6,7 +6,7 @@ import 'package:productos_app/models/models.dart';
 
 class ProductsService extends ChangeNotifier {
 
-final String _baseUrl = 'flutter-varios-c2850-default-rtdb.europe-west1.firebasedatabase.app';
+final String _baseUrl = 'https://flutter-varios-c2850-default-rtdb.europe-west1.firebasedatabase.app';
 final List<Product> products = [];
 
 bool isLoading = true;
